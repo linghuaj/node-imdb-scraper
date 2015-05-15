c2f777e67580232a7f115797fbc69de6b95d5a5f
Linghua Jin <ljin@walmartlabs.com>
2015-05-15 15:01:38 -0700

    clean up

--- index.js
+++ index.js
@@ -7,25 +7,22 @@ require('songbird')
 
 app.get('/scrape', (req, res) => {
     let url = 'http://www.imdb.com/title/tt1229340/'
-    let json = {
-        title: "",
-        release: ""
-    }
     async() => {
         try {
             let [, html] = await request.promise(url)
-            // if you would like to see the full context of the html.
-            // await fs.promise.writeFile('html.txt', html)
             let $ = cheerio.load(html)
-            $('.header').filter(function() {
-                let data = $(this)
-                json.title = data.children().first().text()
-                json.release = data.children().last().children().text()
-            })
+                //get property via cheerio selectors
+            let json = {
+                    title: $('.header .itemprop').text(),
+                    release: $('.header .nobr').children().text(),
+                    rating: $('.titlePageSprite.star-box-giga-star').text()
+                }
+            //write to file
             await fs.promise.writeFile('output.json', JSON.stringify(json, null, 4))
             res.send('Check your console!')
         } catch (e) {
             console.log("error", e)
+            res.send('error')
         }
     }()
 })
